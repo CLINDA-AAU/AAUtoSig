5e1088764ff22f080ef345c7be7897e111f3b985
idabue <ida.egendal@live.dk>
2021-12-08 13:06:16 +0100

    Training + extra enc. layer

--- AAUtoSig.py
+++ AAUtoSig.py
@@ -29,31 +29,142 @@ testloader = torch.utils.data.DataLoader(x_test_tensor, batch_size=8)
 
 # Creating linear (NMF autoencoder)
 # 96 ==> 8 ==> 96
-class NMFAE(torch.nn.Module):
-    def __init__(self, dim):
+class AAUtoSig(torch.nn.Module):
+    def __init__(self, dim1, dim2):
         super().__init__()
 
         
         # Building an linear encoder
         # 96 => dim
-        self.enc1 = torch.nn.Sequential(
-            torch.nn.Linear(96, dim, bias = False),
-            torch.nn.Identity() #also a placeholder for cooler func
-            )
+        self.enc1 = torch.nn.Linear(96, dim1, bias = False)
+        self.enc2 = torch.nn.Linear(dim1, dim2, bias = False)
           
         # Building an linear decoder 
         # dim ==> 96
-        self.dec1 = torch.nn.Sequential(
-            torch.nn.Linear(dim, 96, bias = False),
-            torch.nn.Identity()
-        )
-  
-    #def forward(self, x):
-    #    encoded = self.encoder(x)
-    #    decoded = self.decoder(encoded)
-    #    return decoded
+        self.dec1 = torch.nn.Linear(dim2, 96, bias = False)
+            
 
     def forward(self, x):
-        x = self.enc1(x)
-        x = self.dec1(x)
+        x = F.softplus(self.enc1(x))
+        x = F.softplus(self.enc2(x))
+        x = F.softplus(self.dec1(x))
         return x
+    
+# Model Initialization
+n_sigs = 5
+model = AAUtoSig(dim1 = 30, dim2 = n_sigs)
+
+# Validation using MSE Loss function
+loss_function = torch.nn.MSELoss(reduction='mean')
+#loss_function = torch.nn.KLDivLoss()
+
+
+# Using an Adam Optimizer with lr = 0.1
+optimizer = torch.optim.Adam(model.parameters(),
+                             lr = 1e-3,
+                             weight_decay = 1e-8)
+
+
+#Train
+epochs = 500
+outputs = []
+
+training_plot=[]
+validation_plot=[]
+
+last_score=np.inf
+max_es_rounds = 50
+es_rounds = max_es_rounds
+best_epoch= 0
+#l1_lambda = 0.001
+
+for epoch in range(epochs):
+    model.train()
+    
+    for data in trainloader:
+      # Output of Autoencoder
+      reconstructed = model(data.view(-1,96))
+        
+      # Calculating the loss function
+      loss = loss_function(reconstructed, data.view(-1,96))
+      
+      # l1_norm = sum(p.abs().sum()
+      #            for p in model.parameters())
+ 
+      # loss = loss + l1_lambda * l1_norm
+      
+      # The gradients are set to zero,
+      # the the gradient is computed and stored.
+      # .step() performs parameter update
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      #W = model.dec1.weight.data
+    # print statistics
+    with torch.no_grad():
+        for p in model.parameters():
+            p.clamp_(min = 0)
+        valid_loss=0
+        train_loss=0
+        model.eval()
+        
+        inputs = x_train_tensor[:]
+        outputs = model(inputs)
+        
+        train_loss = loss_function(inputs, outputs)
+        training_plot.append(train_loss)
+    
+ 
+
+        inputs  = x_test_tensor
+        outputs = model(inputs)
+        valid_loss = loss_function(inputs, outputs)
+      
+        validation_plot.append(valid_loss)
+        print("Epoch {}, training loss {}, validation loss {}".format(epoch, 
+                                                                      np.round(training_plot[-1],2), 
+                                                                      np.round(validation_plot[-1],2)))
+
+ #Patient early stopping - thanks to Elixir  
+    if last_score > valid_loss:
+        last_score = valid_loss
+        best_epoch = epoch
+        es_rounds = max_es_rounds
+        best_model = copy.deepcopy(model)
+    else:
+        if es_rounds > 0:
+            es_rounds -=1
+        else:
+            print('EARLY-STOPPING !')
+            print('Best epoch found: nº {}'.format(best_epoch))
+            print('Exiting. . .')
+            break
+
+plt.figure(figsize=(16,12))
+plt.subplot(3, 1, 1)
+plt.title('Score per epoch')
+plt.ylabel('Kullback Leibler Divergence')
+plt.plot(list(range(len(training_plot))), validation_plot, label='Validation DKL')
+plt.plot(list(range(len(training_plot))), training_plot, label='Train DKL')
+plt.legend()
+
+
+def plotsigs(context, mutation, intensities):
+    colors = {'C>A': 'r', 'C>G': 'b', 'C>T': 'g', 
+              'T>A' : 'y', 'T>C': 'c','T>G' : 'm' }
+    plt.figure(figsize=(20,5))
+    plt.bar(x = context, 
+            height =  intensities/np.sum(intensities), 
+            color = [colors[i] for i in mutation])
+    labels = list(colors.keys())
+    handles = [plt.Rectangle((0,0),1,1, color=colors[label]) for label in labels]
+    plt.legend(handles,labels)
+    plt.xticks(rotation=90)
+
+
+W = best_model.dec1.weight.data    
+W_array = W.numpy()
+
+
+for i in range(n_sigs):
+    plotsigs(context, mutation, W_array[:,i])    
