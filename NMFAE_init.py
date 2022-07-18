038aec9194febc1bd581031fa40a126aa0d24738
idabue <ida.egendal@live.dk>
2022-07-18 13:58:15 +0200

    make pots for poster

--- NMFAE_init.py
+++ NMFAE_init.py
@@ -5,7 +5,9 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import copy
+
+from functions import simulate_counts, plotsigs
+from egpm import EGPM
 
 
 class NMFAE(torch.nn.Module):
@@ -39,16 +41,18 @@ def train_NMFAE(epochs, model, x_train, loss_function, optimizer, batch_size):
                                               batch_size=batch_size, 
                                               shuffle=True)
     
-    last_score=np.inf
+    loss_list = []
     for epoch in range(epochs):
         model.train()
         
+        loss_p = 0
         for data in trainloader:
           # Output of Autoencoder
           reconstructed = model(data)#.view(-1,96))
             
           # Calculating the loss function
-          loss = loss_function(reconstructed, data)#.view(-1,96))
+          loss = - loss_function(reconstructed, data)#.view(-1,96))
+          loss_p =+ loss.item()
 
           optimizer.zero_grad()
           loss.backward()
@@ -56,10 +60,48 @@ def train_NMFAE(epochs, model, x_train, loss_function, optimizer, batch_size):
         with torch.no_grad():
             for p in model.parameters():
                 p.clamp_(min = 0)
+        loss_list.append(loss_p)
 
-        '''
-        if last_score > loss:
-            last_score = loss
-            best_model = copy.deepcopy(model)
-        '''
+    plt.plot(range(epochs), loss_list)
+    plt.show() 
     return(model)
+
+
+nsigs = 5
+
+mf_df, true_sigs,_ = simulate_counts(nsigs, 5000, pentanucelotide = False)
+trinucleotide = mf_df.index
+mutation = [t[2:5] for t in trinucleotide]
+
+X = mf_df.T
+
+#80/20 train/validation split
+x_train = X.sample(frac=0.8)
+x_val = X.drop(x_train.index)
+
+#choosing the 'true' number of signatures
+model = NMFAE(dim1 = nsigs)
+
+# Validation using MSE Loss function
+loss_function = torch.nn.MSELoss(reduction='mean')
+
+# Using an Adam Optimizer with lr = 1e-3
+optimizer_enc = EGPM(model.dec1.parameters(), lr = 0.0001 , u_scaling=100, 
+                       norm_per=None, gradient_clipping=True, 
+                       weight_regularization=None, plus_minus=False,
+                       init='bootstrap')
+
+                            
+train_NMFAE(epochs = 2000, 
+            model = model, 
+            x_train = x_train, 
+            loss_function = loss_function, 
+            optimizer = optimizer_enc,
+            batch_size = 32)
+
+#the weights of the decoding layer (dec1) is where we find the signatures.
+sigs = model.dec1.weight.data    
+sigs = pd.DataFrame(sigs.numpy()) 
+
+plotsigs(trinucleotide, mutation, sigs.to_numpy(), 5, "Estimate")
+plotsigs(trinucleotide, mutation, true_sigs.to_numpy(), 5, "True")
