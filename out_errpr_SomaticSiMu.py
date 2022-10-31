b399f995973e4cc1ba3b7be78c0ebb0f5b80596b
idabue <ida.egendal@live.dk>
2022-10-31 13:55:53 +0100

    make linear and nonlinear version

--- out_errpr_SomaticSiMu.py
+++ out_errpr_SomaticSiMu.py
@@ -14,7 +14,7 @@ from sklearn.decomposition import NMF
 
 from random import sample
 
-from functions import cosine_HA
+from functions import cosine_HA, simulate_counts
 from AAUtoSig_init import AAUtoSig, train_AAUtoSig
 import optuna
 from optuna_opt import optuna_tune
@@ -23,7 +23,8 @@ from joblib import Parallel, delayed
 import multiprocessing
 
 os.chdir("../..")
-#os.chdir(r"dfs_forskning/AUH-HAEM-FORSK-MutSigDLBCL222/article_1")
+os.chdir(r"dfs_forskning/AUH-HAEM-FORSK-MutSigDLBCL222/article_1")
+'''
 m = pd.read_csv(r"generated_data/SomaticSiMu/Lung-SCC_SBS96.csv")#.drop(['Unnamed: 0'], axis=1)
 
 groundtruth = [line.strip("\n") for line in open(r"generated_data/SomaticSiMu/Lung-SCC_sbs_sigs.txt")]
@@ -34,8 +35,9 @@ os.chdir(r"scripts/AAUtoSig")
 
 
 sig_names = np.unique(groundtruth.drop([0], axis = 1).fillna(":("))[1::]
+'''
 COSMIC = pd.read_csv("COSMIC/COSMIC_v3.2_SBS_GRCh37.txt", sep = '\t', index_col=0)
-sigs = COSMIC[sig_names]
+#sigs = COSMIC[sig_names]
 
 def split_data(data, frac_1):
   x_train = data.sample(frac = frac_1)
@@ -101,46 +103,47 @@ def out_error_AAUtoSig(train_df, validation_df, nsigs, true_sigs, criterion, opt
    return cos_mean, out_error
     
   
-def performance_analysis(m, m_sigs, COSMIC, criterion, optimizer_alg):
-   m = m.sort_values('0')
-   mut_matrix = (m.drop(['Unnamed: 0', '0'], axis = 1)).T.sample(400)
+def performance_analysis(m, signatures, COSMIC, criterion, optimizer_alg ):#m_sigs, COSMIC, criterion, optimizer_alg):
+   mut_matrix = m.sample(600)
+   #m = m.sort_values('0')
+   #mut_matrix = (m.drop(['Unnamed: 0', '0'], axis = 1)).T.sample(400)
    train_data, test_data = split_data(mut_matrix, 0.8)
   
    idx_train = train_data.index
    idx_test = test_data.index
-   y = [str(x) for x in m_sigs[0]]
+   #y = [str(x) for x in m_sigs[0]]
 
-   train_idx = list (set(idx_train) & set(y))
-   test_idx = list(set(idx_test) & set(y))
-   train_data = train_data[train_data.index.isin(train_idx)]/train_data.max().max()
-   test_data = test_data[test_data.index.isin(test_idx)]/train_data.max().max()
+   #train_idx = list (set(idx_train) & set(y))
+   #test_idx = list(set(idx_test) & set(y))
+   #train_data = train_data[train_data.index.isin(train_idx)]/train_data.max().max()
+   #test_data = test_data[test_data.index.isin(test_idx)]/train_data.max().max()
 
-   m_sigs_train = m_sigs[m_sigs[0].isin([int(x) for x in train_idx])]
-   m_sigs_test = m_sigs[m_sigs[0].isin([int(x) for x in test_idx])]
-   sig_names_train = np.unique(m_sigs_train.drop([0], axis = 1).fillna(":("))[1::]
-   sig_names_test = np.unique(m_sigs_test.drop([0], axis = 1).fillna(":("))[1::]
+   #m_sigs_train = m_sigs[m_sigs[0].isin([int(x) for x in train_idx])]
+   #m_sigs_test = m_sigs[m_sigs[0].isin([int(x) for x in test_idx])]
+   #sig_names_train = np.unique(m_sigs_train.drop([0], axis = 1).fillna(":("))[1::]
+   #sig_names_test = np.unique(m_sigs_test.drop([0], axis = 1).fillna(":("))[1::]
    
-   nsigs = len(sig_names_train)
+   nsigs = 6 #len(sig_names_train)
    #COSMIC = pd.read_csv(r'COSMIC\COSMIC_v3.1_SBS_GRCh38.txt', sep = '\t', index_col=0)
    #old = COSMIC['SBS1']
    #Arrange COSMIC to be the same ordering as count data
    COSMIC = COSMIC.sort_index()
 
-   sigs_train = COSMIC[sig_names_train]
+   #sigs_train = COSMIC[sig_names_train]
 
-   res_NMF = out_errorNMF(train_data, test_data, nsigs, sigs_train, criterion)
+   res_NMF = out_errorNMF(train_data, test_data, nsigs,  signatures, criterion)#sigs_train, criterion)
 
    if criterion == "KL":
       criterion_AE = torch.nn.KLDivLoss(reduction = 'batchmean')
    if criterion == "MSE":
       criterion_AE = torch.nn.MSELoss()
    
-   res_AE = out_error_AAUtoSig(train_data, test_data, nsigs, sigs_train, criterion_AE, optimizer_alg)
+   res_AE = out_error_AAUtoSig(train_data, test_data, nsigs, signatures, criterion_AE, optimizer_alg)# sigs_train, criterion_AE, optimizer_alg)
    return(res_NMF + res_AE)
 
 
 
-data = (m.drop(['Unnamed: 0', '0'], axis = 1)).T
+#data = (m.drop(['Unnamed: 0', '0'], axis = 1)).T
 def analysis_to_plot(data, true_sigs, library, criterion, optimizer_alg, n_sims):
    res = [performance_analysis(data, true_sigs, library, criterion, optimizer_alg) for _ in range(n_sims)]   
    result = pd.DataFrame(res)
@@ -150,7 +153,8 @@ def analysis_to_plot(data, true_sigs, library, criterion, optimizer_alg, n_sims)
    
    #os.chdir("..")
    
-   name = "Lung_SCC_" + str(criterion) + "_" + str(optimizer_alg) + "_nsim:" + str(n_sims)
+   #name = "Lung_SCC_" + str(criterion) + "_" + str(optimizer_alg) + "_nsim:" + str(n_sims)
+   name = "linear" + str(criterion) + "_" + str(optimizer_alg) + "_nsim:" + str(n_sims)
    matplotlib.use('Agg')
    plt.boxplot(result[['outNMF', 'outAE']], labels = ["NMF", "AE"])
    plt.title('NMF vs AE out of sample error on linearly simulated data')
@@ -173,7 +177,19 @@ def analysis_to_plot(data, true_sigs, library, criterion, optimizer_alg, n_sims)
    os.chdir("../../../../..")
 
 
+nsigs = 6
+
+mf_df, true_sigs,_ = simulate_counts(nsigs, 1000, pentanucelotide = False)
+trinucleotide = mf_df.index
+mutation = [t[2:5] for t in trinucleotide]
+
+X = mf_df.T
+print(true_sigs.shape)
+
 params = [("MSE", "Adam"), ("KL", "Adam"), ("MSE", "Tuned")]
-Parallel(n_jobs = 10)(delayed(analysis_to_plot)(m, groundtruth, COSMIC, p[0], p[1], 50) for p in params)
+
+Parallel(n_jobs = 10)(delayed(analysis_to_plot)(X, true_sigs, COSMIC, p[0], p[1], 50) for p in params)
+
+#Parallel(n_jobs = 10)(delayed(analysis_to_plot)(m, groundtruth, COSMIC, p[0], p[1], 50) for p in params)
 
 #analysis_to_plot(m, groundtruth, COSMIC, "MSE", "Adam", 1)
